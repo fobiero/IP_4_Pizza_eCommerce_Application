80479f185c7cb472d22edf603898365686dbb9bb
feLycks <felix.obiero@student.moringaschool.com>
2021-11-29 21:07:47 +0300

    calcuate totals

--- js/script.js
+++ js/script.js
@@ -1,23 +1,95 @@
-class Pizera {
-    constructor(flavour, size, crust, toppings, number, delivery) {
-        this.flavour = flavour;
-        this.size = size;
-        this.crust = crust;
-        this.toppings = toppings;
-        this.number = number;
-        this.delivery = delivery;
-    }
-}
+// get values from the user 
 
-$(document).ready(() => {
-    $('.submit').click((e) => {
-        e.preventDefault()
+$(() => {
+    $('.submit').click(() => {
+        const pFlavors = $('.flavour').val();
+        const pSize = $('.size').val();
+        const pCrust = $('.pcrust').val();
+        const pToppins = $('.p_toppins').val();
+        const pNumber = $('.p_numb').val();
 
-        const flavour = $('.flavour').val();
-        const size = $('.size').val();
-        const crust = $('.crust').val();
-        const toppins = $("toppings").val();
-        const number = $('#number').val();
-        console.log(toppins);
+        class CalcOrder {
+            constructor(flavour, size, crust, toppins, number, total) {
+                this.flavour = flavour;
+                this.size = size;
+                this.crust = crust;
+                this.toppins = toppins;
+                this.number = number;
+                this.total = total;
+            }
+        }
+        let price, totalPrice;
+        switch (pFlavors) {
+            case flavour = 'chicken':
+                switch (pSize) {
+                    case pSize = 'regular':
+                        price = 450;
+                        if (pCrust == 'thin') {
+                            totalPrice = (price * pNumber) + 100;
+                        } else if (pCrust == 'thick') {
+                            totalPrice = (price * pNumber) + 150;
+                        }
+                        break;
+                    case pSize = 'medium':
+                        price = 750;
+                        if (pCrust == 'thin') {
+                            totalPrice = (price * pNumber) + 100;
+                        } else {
+                            totalPrice = (price * pNumber) + 150;
+                        }
+                        break;
+                    case pSize = 'large':
+                        price = 1600;
+                        if (pCrust == 'thin') {
+                            totalPrice = (price * pNumber) + 100;
+                        } else {
+                            totalPrice = (price * pNumber) + 150;
+                        }
+                        break;
+                }
+                break;
+            case flavour = 'veges':
+                switch (size) {
+                    case pSize = 'regular':
+                        price = 350;
+                        if (pCrust == 'thin') {
+                            totalPrice = (price * pNumber) + 100;
+                        } else if (pCrust == 'thick') {
+                            totalPrice = (price * pNumber) + 150;
+                        }
+                        break;
+                    case size = 'medium':
+                        price = 650;
+                        if (pCrust == 'thin') {
+                            totalPrice = (price * pNumber) + 100;
+                        } else {
+                            totalPrice = (price * pNumber) + 150;
+                        }
+                        break;
+                    case size = 'large':
+                        price = 1400;
+                        if (pCrust == 'thin') {
+                            totalPrice = (price * pNumber) + 100;
+                        } else {
+                            totalPrice = (price * pNumber) + 150;
+                        }
+                        break;
+                }
+                break;
+        }
+        // break;
+        switch (pToppins) {
+            case toppins = 'tomato':
+                totalPrice = totalPrice + 75;
+                break;
+            case (toppins) = 'mushroom':
+                totalPrice = totalPrice + 90;
+                break;
+            case toppins = 'beef':
+                totalPrice = totalPrice + 210;
+                break;
+        }
+        const customer = new CalcOrder();
+        console.log(customer);
     })
 })
